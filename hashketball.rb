5ef35038346835b2edd57c60f88eb04b33c4e6da
Juan Carlos Patron <charliepatronr@gmail.com>
2020-07-14 23:39:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -205,9 +205,7 @@ def player_stats(name)
   game_hash.each_with_object({}) do |(key, value), final_hash|
     value.each do |inner_key, inner_value|
       if(inner_key == :players)
-        if(
-          
-          )
+        if()
         binding.pry
       end 
     end
