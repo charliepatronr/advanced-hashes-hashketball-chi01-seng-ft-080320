bbdb644b358df82a446bd8fd72524cbcec12e5ac
Juan Carlos Patron <charliepatronr@gmail.com>
2020-07-14 23:18:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -190,7 +190,7 @@ def player_numbers(team)
     game_hash.each do |home_away_key, outer_value|
       outer_value.each do |team_info, value|
         if(value == team)
-          target_team = value
+          target_team = true
         end 
           if(team_info == :players && target_team == true)
             puts team_info
